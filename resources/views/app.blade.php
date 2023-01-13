21950bde8bb187c7107fcf67f973b6e2ea99df7f
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-14 00:35:44 +0200

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -28,11 +28,27 @@
         <Script>  window.onload = function() { OpenBootstrapPopup(); };function OpenBootstrapPopup() {  $("#exampleModalToggleLabel").modal('hide'); } </script>
         @endsection
 
-        <script type="text/javascript">
-        windown.livewire.on('userStore',()=>{
-            $('#exampleModalToggle').modal('hide');
-        });
+        @push('scripts')
+        <script>
+            window.addEventListener('close-modal', event =>{
+                $('#addStudentModal').modal('hide');
+                $('#editStudentModal').modal('hide');
+                $('#deleteStudentModal').modal('hide');
+            });
+
+            window.addEventListener('show-edit-student-modal', event =>{
+                $('#editStudentModal').modal('show');
+            });
+
+            window.addEventListener('show-delete-confirmation-modal', event =>{
+                $('#deleteStudentModal').modal('show');
+            });
+
+            window.addEventListener('show-view-student-modal', event =>{
+                $('#viewStudentModal').modal('show');
+            });
         </script>
+    @endpush
         </body>
 
     </html>
