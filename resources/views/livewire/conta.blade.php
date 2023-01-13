d49e0ec9103315e5120842a43a1fe89939623a5c
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-13 20:43:23 +0200

    Update conta.blade.php

--- resources/views/livewire/conta.blade.php
+++ resources/views/livewire/conta.blade.php
@@ -71,38 +71,23 @@
                                     <td scope="row">{{ $item->nome }}</td>
                                     <td>{{ $item->idade }}</td>
                                     <td>
-                                        <a class="btn btn-primary" data-bs-toggle="modal"
-                                            data-bs-target="#modalId{{ $item->id }}"><i class="fa fa-eye"></i>
+
+                                        <a class="btn btn-primary" wire:click="edit({{ $item->id }})" data-bs-toggle="modal"
+                                            data-bs-target="#modalId"><i class="fa fa-eye"></i>
                                             Ver</a>
+
+                                        <a class="btn btn-success" wire:click="edit({{ $item->id }})" data-bs-toggle="modal"
+                                            data-bs-target="#modalId"><i class="fa fa-edit"></i>
+                                            Editar</a>
+
                                         <a class="btn btn-danger" wire:click="delete({{ $item->id }})"><i
                                                 class="fa fa-trash"></i> Deletar</a>
+
                                     </td>
                                 </tr>
 
 
-                                <!-- Modal -->
-                                <div class="modal fade" id="modalId{{ $item->id }}" tabindex="-1" role="dialog"
-                                    aria-labelledby="modalTitleId" aria-hidden="true">
-                                    <div class="modal-dialog" role="document">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h5 class="modal-title" id="modalTitleId">Modal title</h5>
-                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                                    aria-label="Close"></button>
-                                            </div>
-                                            <div class="modal-body">
-                                                <div class="container-fluid">
-                                                    {{ $item->nome }}
-                                                </div>
-                                            </div>
-                                            <div class="modal-footer">
-                                                <button type="button" class="btn btn-secondary"
-                                                    data-bs-dismiss="modal">Close</button>
-                                                <button type="button" class="btn btn-primary">Save</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+
                             @endforeach
 
                         </tbody>
@@ -110,6 +95,29 @@
 
                         </tfoot>
                     </table>
+                      <!-- Modal -->
+                      <div wire:ignore.self class="modal fade" id="modalId" tabindex="-1" role="dialog"
+                        aria-labelledby="modalTitleId" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="modalTitleId">Modal title</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                        aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <div class="container-fluid">
+                                        {{ $item->nome }}
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary"
+                                        data-bs-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary">Save</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     {{-- {{ $pessoa->links() }} --}}
                 </div>
             </div>
