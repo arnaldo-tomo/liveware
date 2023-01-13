3a918f45cf428f3997dddf63859b4c5a6833c648
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-13 21:41:03 +0200

    Update conta.blade.php

--- resources/views/livewire/conta.blade.php
+++ resources/views/livewire/conta.blade.php
@@ -46,8 +46,15 @@
         <div class="card">
             <div class="card-header">
                 <div class="row">
+                    @if (session('message'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('message') }}
+                        </div>
+                    @endif
+
                     <div class="col-10">
                         LiveWire
+
                     </div>
                     <div class="col-2 end-100">
                         <button type="submit" data-bs-toggle="modal" href="#exampleModalToggle"
@@ -72,12 +79,12 @@
                                     <td>{{ $item->idade }}</td>
                                     <td>
 
-                                        <a class="btn btn-primary" wire:click="edit({{ $item->id }})" data-bs-toggle="modal"
-                                            data-bs-target="#modalId"><i class="fa fa-eye"></i>
+                                        <a class="btn btn-primary" wire:click="edit({{ $item->id }})"
+                                            data-bs-toggle="modal" data-bs-target="#modalId"><i class="fa fa-eye"></i>
                                             Ver</a>
 
-                                        <a class="btn btn-success" wire:click="edit({{ $item->id }})" data-bs-toggle="modal"
-                                            data-bs-target="#modalId"><i class="fa fa-edit"></i>
+                                        <a class="btn btn-success" wire:click="edit({{ $item->id }})"
+                                            data-bs-toggle="modal" data-bs-target="#modalId"><i class="fa fa-edit"></i>
                                             Editar</a>
 
                                         <a class="btn btn-danger" wire:click="delete({{ $item->id }})"><i
@@ -85,9 +92,6 @@
 
                                     </td>
                                 </tr>
-
-
-
                             @endforeach
 
                         </tbody>
@@ -95,32 +99,35 @@
 
                         </tfoot>
                     </table>
-                      <!-- Modal -->
-                      <div wire:ignore.self class="modal fade" id="modalId" tabindex="-1" role="dialog"
+                    <!-- Modal -->
+                    <div wire:ignore.self class="modal fade" id="modalId" tabindex="-1" role="dialog"
                         aria-labelledby="modalTitleId" aria-hidden="true">
                         <div class="modal-dialog" role="document">
                             <div class="modal-content">
                                 <div class="modal-header">
-                                    <h5 class="modal-title" id="modalTitleId">Modal title</h5>
+                                    <h5 class="modal-title" id="modalTitleId">Update</h5>
                                     <button type="button" class="btn-close" data-bs-dismiss="modal"
                                         aria-label="Close"></button>
                                 </div>
                                 <div class="modal-body">
-                                    <div class="container-fluid">
-                                    <label>Nome</label>
-                                    <input type="hidden" wire:model="user_id">
-                                    <input type="text" class="form-control" wire:model='nome'>
-                                    </div>
-                                    <div class="container-fluid">
-                                        <label>Idade</label>
-                                        <input type="text" class="form-control" wire:model='idade'>
+                                    <form>
+                                        <div class="container-fluid">
+                                            <label>Nome</label>
+                                            <input type="hidden" wire:model="user_id">
+                                            <input type="text" class="form-control" wire:model="nome">
                                         </div>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary"
+                                        <div class="container-fluid">
+                                            <label>Idade</label>
+                                            <input type="text" class="form-control" wire:model="idade">
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary"
                                         data-bs-dismiss="modal">Close</button>
-                                    <button type="button" class="btn btn-primary">Save</button>
-                                </div>
+                                        <button type="button" wire:click.prevent="update()"
+                                        class="btn btn-primary">Save</button>
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>
