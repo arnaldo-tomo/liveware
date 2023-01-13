1d1e7bfcc3e3cb84c9cbeb4fecfcf55420de0aad
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-13 17:35:26 +0200

    Update conta.blade.php

--- resources/views/livewire/conta.blade.php
+++ resources/views/livewire/conta.blade.php
@@ -1,7 +1,7 @@
 <div class="container">
     <h2>Livewore</h2>
     <div class="col-12">
-        <div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel"
+        <div class="modal fade" wire:ignore.self id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel"
             tabindex="-1">
             <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
@@ -10,13 +10,33 @@
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                        Show a second modal and hide this one with the button below.
+                        <div class="row">
+                            <form wire:submit.prevent="save">
+
+                                <div class="col-12">
+                                    <label for="">Nome</label>
+                                    <input type="text" wire:model='nome'class="form-control" value="{{ old('nome') }}"
+                                        placeholder="Informe o nome" aria-describedby="helpId">
+                                    @error('nome')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                                <div class="col-12">
+                                    <label for="">Idade</label>
+                                    <input type="number" wire:model='idade' class="form-control" placeholder="informe a idade"
+                                        aria-describedby="helpId">
+                                    @error('idade')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button class="btn btn-primary" data-bs-dismiss="modal" aria-label="Close">Cancelar</button>
+                            <button class="btn btn-primary" type="submit"><i class="fa fa-save"></i> Regsiatar Produto</button>
+                        </div>
                     </div>
-                    <div class="modal-footer">
-                        <button class="btn btn-primary" data-bs-dismiss="modal" aria-label="Close">Open second modal</button>
-                        <button class="btn btn-primary" data-bs-dismiss="modal" aria-label="Close"><i class="fa fa-cloud"></i>Regsiatar Produto</button>
-                    </div>
-                </div>
+                </form>
             </div>
         </div>
 
@@ -33,70 +53,39 @@
                 </div>
             </div>
             <div class="card-body">
-                <h4 class="card-title">Title</h4>
-                <p class="card-text">Text</p>
+                <div class="table-responsive">
+                    <table class="table table-striped">
+                        <thead class="table-light">
+                            <tr>
+                                <th>Nome</th>
+                                <th>Idade</th>
+                                <th>acction</th>
+                            </tr>
+                        </thead>
+                        <tbody class="table-group-divider">
+                            @foreach ($pessoa as $item)
+                                <tr class="table-primary">
+                                    <td scope="row">{{ $item->nome }}</td>
+                                    <td>{{ $item->idade }}</td>
+                                    <td>
+                                        <a class="btn btn-primary "><i class="fa fa-edit"></i> Editar</a>
+                                        <a class="btn btn-danger" wire:click="delete({{ $item->id }})"><i
+                                                class="fa fa-trash"></i> Deletar</a>
+                                    </td>
+                                </tr>
+                            @endforeach
+
+                        </tbody>
+                        <tfoot>
+
+                        </tfoot>
+                    </table>
+                    {{-- {{ $pessoa->links() }} --}}
+                </div>
             </div>
             <div class="card-footer text-muted">
-                Footer
+               <i class="fa fa-copyright" aria-hidden="true"></i>  Arnaldo Tomo [] 13.janeiro .2023 <i class="fa fa-calendar"></i>
             </div>
         </div>
 
-        <div class="row">
-            <form wire:submit.prevent="save">
-
-                <div class="col-4">
-                    <label for="">Nome</label>
-                    <input type="text" wire:model='nome'class="form-control" value="{{ old('nome') }}"
-                        placeholder="Informe o nome" aria-describedby="helpId">
-                    @error('nome')
-                        <span class="text-danger">{{ $message }}</span>
-                    @enderror
-                </div>
-                <div class="col-4">
-                    <label for="">Idade</label>
-                    <input type="number" wire:model='idade' class="form-control" placeholder="informe a idade"
-                        aria-describedby="helpId">
-                    @error('idade')
-                        <span class="text-danger">{{ $message }}</span>
-                    @enderror
-                </div>
-                <div class="col-4">
-                    <label for="">Idade</label>
-                    <br>
-                    <button class="btn btn-primary" type="submit">Salvar</button>
-                </div>
-            </form>
-        </div>
-        <hr>
-        <div class="card-head">
-            <div class="table-responsive mb-2">
-                <table class="table table-striped">
-                    <thead class="table-light">
-                        <tr>
-                            <th>Nome</th>
-                            <th>Idade</th>
-                            <th>acction</th>
-                        </tr>
-                    </thead>
-                    <tbody class="table-group-divider">
-                        @foreach ($pessoa as $item)
-                            <tr class="table-primary">
-                                <td scope="row">{{ $item->nome }}</td>
-                                <td>{{ $item->idade }}</td>
-                                <td>
-                                    <a class="btn btn-primary "><i class="fa fa-edit"></i></a>
-                                    <a class="btn btn-danger" wire:click="delete({{ $item->id }})"><i
-                                            class="fa fa-trash"></i></a>
-                                </td>
-                            </tr>
-                        @endforeach
-
-                    </tbody>
-                    <tfoot>
-
-                    </tfoot>
-                </table>
-                {{-- {{ $pessoa->links() }} --}}
-            </div>
-        </div>
     </div>
