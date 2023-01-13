d6ef5db346115ce16b3156eb872cf2038bed56db
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-13 16:05:25 +0200

    Create conta.blade.php

---
+++ resources/views/livewire/conta.blade.php
@@ -0,0 +1,52 @@
+<div class="container">
+<h2>Livewore</h2>
+    <div class="col-12">
+        <div class="row">
+            <form wire:submit.prevent="save">
+        <div class="col-4">
+          <label for="">Nome</label>
+          <input type="text" wire:model='nome'class="form-control" placeholder="" aria-describedby="helpId">
+          <small id="helpId" class="text-muted">Help text</small>
+        </div>
+        <div class="col-4">
+          <label for="">Idade</label>
+          <input type="number" wire:model='idade'  class="form-control" placeholder="" aria-describedby="helpId">
+          <small id="helpId" class="text-muted">Help text</small>
+        </div>
+        <div class="col-4">
+            <label for="">Idade</label>
+            <br>
+        <button class="btn btn-primary"  type="submit">Salvar</button>
+        </div>
+    </form>
+</div>
+<hr>
+    <div class="table-responsive mb-2">
+        <table class="table table-striped">
+            <thead class="table-light">
+                <tr>
+                    <th>Nome</th>
+                    <th>Idade</th>
+                    <th>acction</th>
+                </tr>
+                </thead>
+                <tbody class="table-group-divider">
+                    @foreach ($pessoa as $item)
+
+                    <tr class="table-primary" >
+                        <td scope="row">{{ $item->nome }}</td>
+                        <td>{{ $item->idade }}</td>
+                        <td>
+                            <a class="btn btn-primary "><i class="fa fa-edit"></i></a>
+                            <a class="btn btn-danger "><i class="fa fa-trash"></i></a>
+                        </td>
+                    </tr>
+                    @endforeach
+
+                </tbody>
+                <tfoot>
+
+                </tfoot>
+        </table>
+    </div>
+</div>
