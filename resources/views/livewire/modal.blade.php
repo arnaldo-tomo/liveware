493323271d58ce5214e7005b55e0cd6e8bab404f
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-15 00:33:37 +0200

    Update modal.blade.php

--- resources/views/livewire/modal.blade.php
+++ resources/views/livewire/modal.blade.php
@@ -69,6 +69,34 @@
 </div>
 
 
+
+<!-- Modal Body-->
+<div wire:ignore.self class="modal fade " id="delete{{ $item->id }}" tabindex="-2" role="dialog" aria-labelledby="modalTitleId" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+                <div class="modal-header bg-danger">
+                        <h5 class="modal-title" id="modalTitleId">DELETDAE</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+            <div class="modal-body">
+                <div class="container-fluid">
+                    {{ $item->nome }}
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary" wire:click="delete({{ $item->id }})">Save</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
 @push('component-scripts')
 
 <script>
