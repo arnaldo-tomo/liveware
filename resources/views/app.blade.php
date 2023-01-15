034a5ddef8150f37c4d584a29bdb0c1f23696a91
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-15 22:21:25 +0200

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,24 +6,24 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="css/all.min.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" />
-        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+
     </head>
     @livewireStyles
+
     <body>
-{{-- <livewire:conta> --}}
+        {{-- <livewire:conta> --}}
         {{ $slot }}
 
         <script src="js/bootstrap.bundle.min.js"></script>
-        <script src="js/popper.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-        <script src="js/jquery-3.6.0.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js"></script>
 
+        @stack('component-scripts')
 
-@stack('component-scripts')
+        @livewireScripts
 
-          @livewireScripts
-
-        </body>
+    </body>
 
     </html>
-
